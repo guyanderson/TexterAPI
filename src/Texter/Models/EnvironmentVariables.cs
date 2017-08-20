ec62ee68778cab7ca090cf0dac4b69df45feb111
guy <luckythelich@hotmail.com>
2017-08-19 21:12:45 -0700

    not getting a verification text.

---
+++ src/Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,8 @@
+﻿namespace Texter.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string AccountSid = "AC8f1ff03903ea3aaca2b2ec0c2e2e1a11";
+        public static string AuthToken = "fc91c3dcc8e1bc5faf658339bf7d9fa0";
+    }
+}
